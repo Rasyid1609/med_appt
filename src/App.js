5e3f5bd8d49890c84b1c66ae8dc558b3af6a744f
Rasyid1609 <rasyid.mahardian@gmail.com>
2023-12-30 17:25:45 +0700

    Capstone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Notification from './Components/Notification/Notification';
+import Login from './Components/Login/Login';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import BookingConsultation from './Components/BookingConsultation';
+import ReviewForm from './Components/ReviewForm/ReviewForm';
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Notification>
+      <Routes>
+        <Route path='/' element={<Landing_Page/>} />
+        <Route path='/Sign_Up' element={<Sign_Up/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/instant-consultation' element={<InstantConsultation/>}/>
+        <Route path='/finddoctor' element={<FindDoctorSearch/>} />
+        <Route path='/search/doctors' element={<BookingConsultation/>} />
+        <Route path='/reviews' element={<ReviewForm/>} />
+      </Routes>
+      </Notification>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/ReviewForm/ReviewForm.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+import "./ReviewForm.css";
+import GiveReviews from './GiveReviews';
+
+const ReviewForm = () => {
+    const [reviewData, setReviewData] = useState({});
+
+    const reportData = [
+        {
+            serialNumber: 1,
+            doctorName: 'Dr. Ramesh',
+            doctorSpeciality: 'Cardiology',
+
+        },
+        {
+            serialNumber: 2,
+            doctorName: 'Dr. Harini',
+            doctorSpeciality: 'Dermatology',
+
+        },
+    ];
+
+    const handleGiveReview = (serialNumber) => {
+        setReviewData((prevReviewData) => ({
+            ...prevReviewData,
+            [serialNumber]: ''
+        }));
+    };
+
+    const handleReviewSubmit = (serialNumber, review) => {
+        setReviewData((prevReviewData) => ({
+            ...prevReviewData,
+            [serialNumber]: review
+        }));
+    };
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        //   const authtoken = sessionStorage.getItem("auth-token");
+        //   if (!authtoken) {
+        //       navigate("/login");
+        //   }
+    }, []);
+
+  return (
+    <div style={{ marginTop: '10%' }} className="reviews-container">
+            <h1>Reviews</h1>
+            <table className="report-table">
+                <thead>
+                    <tr>
+                        <th>S.No.</th>
+                        <th>Doctor Name</th>
+                        <th>Doctor Specialty</th>
+                        <th>Provide Feedback</th>
+                        <th>Review Given</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {reportData.map((report) => (
+                        <tr key={report.serialNumber}>
+                            <td>{report.serialNumber}</td>
+                            <td>{report.doctorName}</td>
+                            <td>{report.doctorSpeciality}</td>
+                            <td>
+                                {!reviewData[report.serialNumber] ? (
+                                    <Popup
+                                        trigger={
+                                            <button
+                                                className="give-review-button"
+                                                onClick={() => handleGiveReview(report.serialNumber)}
+                                            >
+                                                Click Here
+                                            </button>
+                                        }
+                                        modal
+                                        nested
+                                    >
+                                        {(close) => (
+                                            <div className="modal">
+                                                <GiveReviews
+                                                    serialNumber={report.serialNumber}
+                                                    onReviewSubmit={handleReviewSubmit}
+                                                    review={reviewData[report.serialNumber]} // Pass the review data
+                                                />
+                                                <button className="close-modal-button" onClick={close}>
+                                                    Close
+                                                </button>
+                                            </div>
+                                        )}
+                                    </Popup>
+                                ) : (
+                                    <button className="give-review-button" disabled>
+                                        Give Review
+                                    </button>
+                                )}
+                            </td>
+                            <td>
+                                {reviewData[report.serialNumber] && (
+                                    <div className="review-given">
+                                        <p>{reviewData[report.serialNumber]}</p>
+                                    </div>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+  );
+};
+
+export default ReviewForm;
